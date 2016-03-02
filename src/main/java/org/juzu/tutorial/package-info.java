ffa8c0cab8a28e74057f7356642ad0481806b5b9
Danny Lai <dannyl@corp.capcom.co.jp>
2016-03-01 17:34:35 -0800

    improved gui

--- src/main/java/org/juzu/tutorial/package-info.java
+++ src/main/java/org/juzu/tutorial/package-info.java
@@ -31,9 +31,27 @@ package org.juzu.tutorial;
   @Binding(value = org.juzu.tutorial.services.SecretService.class, implementation = org.juzu.tutorial.services.SercretServiceMemImpl.class,
       scope = juzu.Scope.SINGLETON)
 })
+@Less(@Stylesheet("styles/juzret.less"))
+
+@WebJars(@WebJar("jquery"))
+@Scripts(
+    {
+      @Script(id="jquery", value="jquery/1.10.2/jquery.js"),
+      @Script(value="javascripts/secret.js", depends = "jquery")
+    }
+)
+
+@Assets("*")
 package org.juzu.tutorial;
 
 import juzu.Scope;
+import juzu.plugin.asset.Assets;
+import juzu.plugin.asset.Script;
+import juzu.plugin.asset.Scripts;
+import juzu.plugin.asset.Stylesheet;
 import juzu.plugin.binding.Binding;
 import juzu.plugin.binding.Bindings;
+import juzu.plugin.less4j.Less;
+import juzu.plugin.webjars.WebJar;
+import juzu.plugin.webjars.WebJars;
 
